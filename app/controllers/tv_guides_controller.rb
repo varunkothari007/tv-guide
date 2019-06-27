e894637b58ed724218601c0056572a78dde786d7
Varun Kothari <varun.kothari007@gmail.com>
2019-06-27 20:38:52 +0530

    Added will paginate

--- app/controllers/tv_guides_controller.rb
+++ app/controllers/tv_guides_controller.rb
@@ -1,7 +1,7 @@
 class TvGuidesController < ApplicationController
   def index
-  	@transmitters = Transmitter.all
+  	@transmitters = Transmitter.paginate(page: params[:page], per_page: 20)
 
-  	@episode = Episode.all
+  	@episode = Episode.paginate(page: params[:page], per_page: 30)
   end
 end
