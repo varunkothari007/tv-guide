40e1aa9b678f2a6210b4a7dac7a764bb96544d68
Bhavesh <bhaveshkshatriya.ror@gmail.com>
2019-07-19 14:39:22 +0530

    Cleanup not used code.

--- app/controllers/tv_guides_controller.rb
+++ app/controllers/tv_guides_controller.rb
@@ -1,6 +1,5 @@
 class TvGuidesController < ApplicationController
   def index
-  	@transmitters = Transmitter.paginate(page: params[:page], per_page: 20)
 
   	@episode = Episode.paginate(page: params[:page], per_page: 30)
   end
